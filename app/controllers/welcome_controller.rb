93c7ca7e4f994d0d78ef7b55cbec68fe0ad49b0c
Nick Yee <njsyee@gmail.com>
2016-03-11 06:52:55 -0700

    Kept client_id and _secret hidden but accessible to rails app

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,14 +3,15 @@ class WelcomeController < ApplicationController
     require 'oauth2'
     site_path = 'https://branchoutdev.nationbuilder.com'
     redirect_uri = 'http://localhost:3000/oauth_callback'
-    client_id = ENV['CLIENT_ID']
-    client_secret = ENV['CLIENT_SECRET']
+    client_id = OAUTH_CONFIG['client_id']
+    client_secret = OAUTH_CONFIG['client_secret']
     client = OAuth2::Client.new(
       client_id, 
       client_secret,
       site: site_path
     )
 
+    @client_id = client_id
     @oauth_authorize = client.auth_code.authorize_url(redirect_uri: redirect_uri)
   end
 end
