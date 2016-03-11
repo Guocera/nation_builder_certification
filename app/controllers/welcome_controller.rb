d34b801a96fe650e44a17b1b028377fbcf83ff7f
Nick Yee <njsyee@gmail.com>
2016-03-11 06:40:22 -0700

    Added a link to request authorization; doesn't work yet

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     require 'oauth2'
-    site_path = 'https://foobar.nationbuilder.com'
+    site_path = 'https://branchoutdev.nationbuilder.com'
     redirect_uri = 'http://localhost:3000/oauth_callback'
     client_id = ENV['CLIENT_ID']
     client_secret = ENV['CLIENT_SECRET']
@@ -10,7 +10,7 @@ class WelcomeController < ApplicationController
       client_secret,
       site: site_path
     )
-    
+
     @oauth_authorize = client.auth_code.authorize_url(redirect_uri: redirect_uri)
   end
 end
