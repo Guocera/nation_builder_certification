a25e8bec3869d0baf85c988c33e5c5b1fb79d291
Nick Yee <njsyee@gmail.com>
2016-03-14 07:07:11 -0600

    Created a method, #get_access_token  to get the access token

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -1,21 +1,7 @@
 class OauthsController < ApplicationController
-  def new
-    require 'oauth2'
-    
-    code = params[:code]
-    site_path = 'https://branchoutdev.nationbuilder.com'
-    redirect_uri = 'http://localhost:3000/oauth/new'
-    client_id = OAUTH_CONFIG['client_id']
-    client_secret = OAUTH_CONFIG['client_secret']
-    client = OAuth2::Client.new(
-      client_id, 
-      client_secret,
-      site: site_path
-    )
+  def new    
+    session[:code] = params[:code]
 
-    token = client.auth_code.get_token(code, redirect_uri: redirect_uri)
-    session[:code] = code
-    session[:token] = token
     redirect_to root_path
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     require 'oauth2'
+    require 'json'
+
     site_path = 'https://branchoutdev.nationbuilder.com'
     redirect_uri = 'http://localhost:3000/oauth/new'
     client_id = OAUTH_CONFIG['client_id']
@@ -11,8 +13,13 @@ class WelcomeController < ApplicationController
       site: site_path
     )
 
-    @code = session[:code]
-    @token = session[:token]
+    code = session[:code]
+    if code
+      token = get_access_token(code)
+      response = token.get('/api/v1/people', headers: { "Accept": "application/json" }, params: { page: 1 } )      
+      @people = JSON.parse(response.body)
+    end
+
     @client_id = client_id
     @oauth_authorize = client.auth_code.authorize_url(redirect_uri: redirect_uri)
   end
