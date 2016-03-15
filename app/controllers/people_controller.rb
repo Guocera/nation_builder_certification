9e7b1df60ad3f5ca19f9b36bb307b5acdd4bb8f2
Nick Yee <njsyee@gmail.com>
2016-03-15 07:29:52 -0600

    Can retrieve token on any page by reserializing the token hash using a new client

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -4,8 +4,18 @@ class PeopleController < ApplicationController
     # response = token.get('/api/v1/people', headers: { "Accept": "application/json" }, params: { page: 1 } )      
     # @people = JSON.parse(response.body)
 
-    @token = session[:token]
-    @client = session[:client]
+ 
+    site_path = 'https://branchoutdev.nationbuilder.com'
+    client_id = OAUTH_CONFIG['client_id']
+    client_secret = OAUTH_CONFIG['client_secret']
+    client = OAuth2::Client.new(
+      client_id, 
+      client_secret,
+      site: site_path
+    )
+
+    @token = OAuth2::AccessToken.from_hash(client, session[:token]).inspect
+    @client = client.inspect
   end
   
 end
