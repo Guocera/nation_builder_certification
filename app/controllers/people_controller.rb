b33c1644a36fc8430325c3edf87dbd5db839841e
Nick Yee <njsyee@gmail.com>
2016-03-15 07:38:00 -0600

    Added schema that was created earlier

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -5,7 +5,7 @@ class PeopleController < ApplicationController
     client = get_client
     token = OAuth2::AccessToken.from_hash(client, session[:token])
 
-    response = token.get('/api/v1/people', headers: { "Accept": "application/json" }, params: { page: 1 } )      
+    response = token.get('/api/v1/people', headers: { "Accept": "application/json" }, params: { page: 1 } )
     @people = JSON.parse(response.body)
 
   end
