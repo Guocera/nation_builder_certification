377807693a698791612858c9e36688a3a252213b
Nick Yee <njsyee@gmail.com>
2016-03-16 06:01:06 -0600

    Recreated events table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160316114633) do
+ActiveRecord::Schema.define(version: 20160316115414) do
+
+  create_table "events", force: :cascade do |t|
+    t.string   "status"
+    t.string   "name"
+    t.string   "intro"
+    t.string   "time_zone"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.string   "contact_name"
+    t.string   "contact_contact_phone"
+    t.boolean  "contact_show_phone"
+    t.string   "contact_contact_email"
+    t.string   "contact_email"
+    t.boolean  "contact_show_email"
+    t.string   "rsvp_phone"
+    t.string   "rsvp_address"
+    t.boolean  "rsvp_allow_guests"
+    t.boolean  "rsvp_accept_rsvps"
+    t.boolean  "rsvp_gather_volunteers"
+    t.boolean  "show_guests"
+    t.integer  "capacity"
+    t.string   "venue_name"
+    t.string   "venue_address_address"
+    t.string   "venue_address_city"
+    t.string   "venue_address_state"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string   "email"
