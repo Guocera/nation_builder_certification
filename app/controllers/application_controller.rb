bb2c198e56fee281643577b2e50c52daaa32cf3c
Nick Yee <njsyee@gmail.com>
2016-03-23 05:18:42 -0600

    Moved code for client and redirect url to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  require 'oauth2'
 
   private
 
@@ -20,5 +21,20 @@ class ApplicationController < ActionController::Base
     token = client.auth_code.get_token(code, redirect_uri: redirect_uri).to_hash
 
     redirect_to redirect_path
+  end
+
+  def get_client
+    site_path = 'https://branchoutdev.nationbuilder.com'
+    client_id = OAUTH_CONFIG['client_id']
+    client_secret = OAUTH_CONFIG['client_secret']
+    client = OAuth2::Client.new(
+      client_id, 
+      client_secret,
+      site: site_path
+    )
+  end
+
+  def get_redirect_url
+    redirect_uri = 'http://localhost:3000/oauth/new'
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,17 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    require 'oauth2'
-
-    site_path = 'https://branchoutdev.nationbuilder.com'
     redirect_uri = 'http://localhost:3000/oauth/new'
-    client_id = OAUTH_CONFIG['client_id']
-    client_secret = OAUTH_CONFIG['client_secret']
-    client = OAuth2::Client.new(
-      client_id, 
-      client_secret,
-      site: site_path
-    )
-
+    client = get_client
     @oauth_authorize = client.auth_code.authorize_url(redirect_uri: redirect_uri)
 
   end
