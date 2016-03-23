81b490a0164372e74cdba5f1262216c9882f2fa4
Nick Yee <njsyee@gmail.com>
2016-03-23 05:47:24 -0600

    @people gives a list of ppl

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,7 +2,8 @@ class PeopleController < ApplicationController
 
   def index
     response = token.get('/api/v1/people', headers: { "Accept": "application/json" }, params: { page: 1 } )
-    @people = JSON.parse(response.body)
+    @people = JSON.parse(response.body)["results"]
+    puts JSON.pretty_generate(@people)
   end
   
   def show
