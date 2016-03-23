ea90151db97411c3f9fc6ad9a7913636bf8b7dc1
Nick Yee <njsyee@gmail.com>
2016-03-23 05:15:30 -0600

    Fixing refresh glitch and cleaning code. Just moved code to generate log in link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,18 +21,4 @@ class ApplicationController < ActionController::Base
 
     redirect_to redirect_path
   end
-
-  def get_request_token
-    site_path = 'https://branchoutdev.nationbuilder.com'
-    redirect_uri = 'http://localhost:3000/oauth/new'
-    client_id = OAUTH_CONFIG['client_id']
-    client_secret = OAUTH_CONFIG['client_secret']
-    client = OAuth2::Client.new(
-      client_id, 
-      client_secret,
-      site: site_path
-    )
-
-    @oauth_authorize = client.auth_code.authorize_url(redirect_uri: redirect_uri)
-  end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,18 @@
 class WelcomeController < ApplicationController
   def index
     require 'oauth2'
-    require 'json'
 
-    @oauth_authorize = get_request_token
-    session[:authenticate] = @oauth_authorize
+    site_path = 'https://branchoutdev.nationbuilder.com'
+    redirect_uri = 'http://localhost:3000/oauth/new'
+    client_id = OAUTH_CONFIG['client_id']
+    client_secret = OAUTH_CONFIG['client_secret']
+    client = OAuth2::Client.new(
+      client_id, 
+      client_secret,
+      site: site_path
+    )
+
+    @oauth_authorize = client.auth_code.authorize_url(redirect_uri: redirect_uri)
 
   end
 end
