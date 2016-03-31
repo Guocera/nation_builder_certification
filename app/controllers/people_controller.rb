d536c691fb2467551b689e9ebd25a56df1bac332
Nick Yee <njsyee@gmail.com>
2016-03-31 05:19:35 -0600

    Got :is_new to show something for new and edit

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,6 +1,7 @@
 class PeopleController < ApplicationController
 
   def index
+    @person = Person.new
     response = token.get('/api/v1/people', headers: { "Accept": "application/json" }, params: { page: 1 } )
     @people = JSON.parse(response.body)["results"]
   end
